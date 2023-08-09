a10bb07ff70ace296da632bf0f76aa672f35559a
Liu siyu <137892154+infinitelarge@users.noreply.github.com>
2023-08-09 20:28:09 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -3,8 +3,9 @@
 set -eux
 
 CITY=Shanghai
-LANGUAGE="zh-CN"
-UNIT=m
-#UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
-curl -o result.html https://wttr.in/Shanghai
+curl \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  https://wttr.in/$CITY
